28ad4d9c03fb785197371f0a12966095d060a06f
winsss123 <58025822+winsss123@users.noreply.github.com>
2019-11-27 16:26:24 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -17,11 +17,11 @@ SERVER_ID = "fe2570c5ce2245c49c3df7755481105e"
 SECRET_KEY_NAME = "private_20191127155858.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://pm191127samplebot.herokuapp.com/"
+LOCAL_ADDRESS = "https://workingbot.herokuapp.com/"
 
 # DB config
-DB_HOST = "ec2-54-235-248-185.compute-1.amazonaws.com"
-DB_NAME = "d948ducpbs8to4"
-DB_USER = "irxbslbpfagcyw"
+DB_HOST = "ec2-174-129-253-1.compute-1.amazonaws.com"
+DB_NAME = "d1elpsgecjt36b"
+DB_USER = "zfcrsamnpnizno"
 DB_PORT = "5432"
-DB_PASSWORD = "4484cc1abc190bd1bc9c46da75ff2f67df260963ee3e87f0eb7fd2eecd3942d3"
+DB_PASSWORD = "85abc60d907de9d3d8aee149cdf79128927d209259222a6df51b87bd659f444b"
